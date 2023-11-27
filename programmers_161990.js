be7c2c881c76067d0accff36b4d9ab1a8e9043ab
Hyobin Choe <95601940+AtomicLiquors@users.noreply.github.com>
2023-11-27 17:35:15 +0900

    Update programmers_161990.js

--- programmers_161990.js
+++ programmers_161990.js
@@ -40,3 +40,27 @@ function manhattan(lux, luy, rdx, rdy){
     return Math.abs(rdx - lux) + Math.abs(rdy - luy);
 }
 */
+
+
+/* 모범답안 : forEach 활용이 인상적이다. 
+배열을 분해하여 Math.min/max 한번만 사용하여 최대/최소를 구하는 것도 마찬가지. 
+다만 +1 연산은 return문에 넣어서 한 번만 실행하는 게 더 낫지 않았을까.*/
+/*
+function solution(wallpaper) {
+    let left = [];
+    let top = [];
+    let right = []
+    let bottom = [];
+    wallpaper.forEach((v,i) => {
+        [...v].forEach((val,ind) => {
+            if(val === "#") {
+                left.push(i)
+                top.push(ind)
+                right.push(i + 1)
+                bottom.push(ind + 1)
+            }
+        })
+    })
+    return [Math.min(...left), Math.min(...top), Math.max(...right), Math.max(...bottom)]
+}
+*/
