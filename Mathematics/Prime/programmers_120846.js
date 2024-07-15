1806493e392ac794c3de40eb5820da94fc172b00
Hyobin Choe <95601940+AtomicLiquors@users.noreply.github.com>
2024-07-15 15:33:48 +0900

    Update programmers_120846.js

--- Mathematics/Prime/programmers_120846.js
+++ Mathematics/Prime/programmers_120846.js
@@ -18,3 +18,19 @@ function isComposite(num){
     }
     return false;
 }
+
+/* 곱을 이용한 합성수 구하기*/
+/*
+function solution(n) {
+    let dp = new Array(n+1).fill(1)
+    for(let i = 2 ; i <= n ; i++){
+        if(dp[i]){
+            for(let j = 2 ; i*j <= n ; j++){
+                dp[i*j] = 0
+            }
+        }
+    }
+
+    return dp.filter(el => el === 0).length
+}
+*/
